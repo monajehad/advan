055f00f79aa0058a4d86d5ba6096365c4b5160ef
monajehad <monajehad2000@gmail.com>
2023-03-01 22:33:27 +0200

    side menu.

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -22,3 +22,17 @@
 
 
 <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+    var path_for_aside_menu = window.location.href;
+    $('.aside-menu .menu-nav  .menu-item  ').filter(function () {
+        return this.href == path_for_aside_menu;
+    }).addClass('menu-item-active');
+
+    $('.aside-menu .menu-item').filter(function () {
+        return $('a', this).attr('href') === path_for_aside_menu;
+    }).closest('.menu-item').addClass('menu-item-open ');
+});
+
+</script>
